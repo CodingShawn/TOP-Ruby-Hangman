e7489abe7fff9b6ea5d1a08fb5a6fe360d31f70b
Shawn <codingshawn@gmail.com>
2020-06-27 19:10:07 +0800

    Added functionality to show current guess with _ for unguessed letters

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -15,17 +15,39 @@ def select_random_word(dictionary)
   return dictionary[rand(dictionary.length - 1)]
 end 
 
-def take_player_input()
+def display_current_guess(chosen_word, correct_guesses = [])
+  display = chosen_word.chars.map { |char| 
+    unless correct_guesses.include? char
+      char = "_"
+    end
+    char
+  }
+  return display
+end
+
+def take_player_input(previous_guess)
   guess = ""
   loop do 
     puts "Please choose a letter"
     guess = gets.chomp
     break if guess =~ /\A[A-Za-z]?\z/ #check if input is a single letter
   end
-  return guess
+  return guess.upcase
 end
 
+def game
+  chosen_word = select_random_word(process_text_file("../5desk.txt"))
+  guesses_left = 8
+  correct_guesses = []
+  wrong_guesses = []
+  while guesses_left > 0
+    guess = take_player_input()
+    if previous_guess.include? guess
+      puts "You have already chosen #{guess} before. Please choose another letter"
+      guess = take_player_input()
+    end 
+    if chosen_word.include? guess
+      puts "You're right!"
+    
 
-#word = select_random_word(process_text_file("../5desk.txt"))
-word = take_player_input
-print word
+
